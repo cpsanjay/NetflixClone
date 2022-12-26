6a5e35ec8c4709e7fb5536421c6cafd20078d2e0
cpsanjay <cpsanjay1999@gmail.com>
2022-12-26 18:30:04 +0530

    firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Account from "./pages/Account";
 
 function App() {
   return (
@@ -8,6 +12,16 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/account"
+          element={
+            <ProtectedRoute>
+              <Account />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBhE9xNKDup_97hoyDwd714A_oqtgQbTCE",
-  authDomain: "netflix-react-codecommerce.firebaseapp.com",
-  projectId: "netflix-react-codecommerce",
-  storageBucket: "netflix-react-codecommerce.appspot.com",
-  messagingSenderId: "358906046176",
-  appId: "1:358906046176:web:69add1013169c6179e1285",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
